edb819cc2c78f04a6b2b2610720100a6d0f04d9e
KwesiNavilot <andrewskwesiankomahene@gmail.com>
2022-01-22 18:19:01 +0000

    Tweaked DB config to allow for all string sizes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         //Use Bootstrap for pagination views
         Paginator::useBootstrap();
 
