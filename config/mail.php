89927eb9e2255b6bf2e05758cede38810e0fcebb
KwesiNavilot <andrewskwesiankomahene@gmail.com>
2022-01-24 20:06:17 +0000

    Added MailJet support

--- config/mail.php
+++ config/mail.php
@@ -45,6 +45,38 @@ return [
             'auth_mode' => null,
         ],
 
+        'mailjet' => [
+            'key' => env('MAILJET_APIKEY'),
+            'secret' => env('MAILJET_APISECRET'),
+            'transactional' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3.1',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+            'common' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+            'v4' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v4',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ]
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
