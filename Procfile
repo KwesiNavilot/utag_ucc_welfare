a83bae136159b79440769fde082320d18263d1c4
KwesiNavilot <andrewskwesiankomahene@gmail.com>
2022-01-25 13:11:04 +0000

    Updated ProcFile with new worker

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: vendor/bin/heroku-php-apache2 public/
+worker: php artisan queue:work  --queue=single-email,default,bulk-members-email,bulk-execs-email database --tries=5
--- config/mail.php
+++ config/mail.php
@@ -79,6 +79,7 @@ return [
             'transport' => 'failover',
             'mailers' => [
                 'smtp',
+                'mailjet',
                 'log',
             ],
         ],
